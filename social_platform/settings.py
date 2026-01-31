cfc97cbdc82cac82d9b91918ae617f7f3044361b
amulyav2004 <amulyav996@gmail.com>
2026-01-31 18:03:19 +0530

    Fix dj_database_url ParseError

--- social_platform/settings.py
+++ social_platform/settings.py
@@ -70,13 +70,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'social_platform.wsgi.application'
 
-# Database configuration
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
+    "default": dj_database_url.config(
+        default=f"sqlite:///{str(BASE_DIR / 'db.sqlite3')}",
+        conn_max_age=600,
+        ssl_require=not DEBUG,
     )
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
